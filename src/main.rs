cec328976802ce1a1aab7cc43a4d93d3287054f5
aagargoura <ahmed.gargoura@gmail.com>
2025-12-28 12:51:12 +0100

    Remove secret number print and enhance error printing

--- src/main.rs
+++ src/main.rs
@@ -8,10 +8,8 @@ fn main() {
 
     let secret_number = rand::thread_rng().gen_range(1..=100);
 
-    println!("The secret number is: {secret_number}");
-
     loop {
-        println!("Please input your guess.");
+        println!("---- Please input your guess:");
 
         let mut guess = String::new();
 
@@ -21,7 +19,10 @@ fn main() {
 
         let guess: u32 = match guess.trim().parse() {
             Ok(num) => num,
-            Err(_) => continue,
+            Err(_) => {
+                println!("Please type a number!");
+                continue;
+            }
         };
 
         println!("You guessed: {guess}");
